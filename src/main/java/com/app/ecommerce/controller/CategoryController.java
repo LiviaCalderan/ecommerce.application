bcaf58f5f36d5afd0b1b1690c906898f59dc4d0f
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-15 18:25:50 -0400

    feat(app): Category Controller - CRUD created

---
+++ src/main/java/com/app/ecommerce/controller/CategoryController.java
@@ -0,0 +1,47 @@
+package com.app.ecommerce.controller;
+
+import com.app.ecommerce.model.Category;
+import com.app.ecommerce.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    @Autowired
+    public final CategoryService categoryService;
+
+    @GetMapping("/public/categories")
+    public ResponseEntity<List<Category> getAllCategories(){
+        return new ResponseEntity<>(categoryService.fetchCategories(), HttpStatus.OK);
+    }
+
+    @PostMapping("/public/categories")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+        categoryService.createCategory();
+        return ResponseEntity.ok("Category added successfully");
+    }
+
+    @DeleteMapping("admin/categories/{categoryId}")
+    public ResponseEntity<Category> deleteCategory(@PathVariable Long categoryId){
+        Category deletedCategory = categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(deletedCategory, HttpStatus.OK);
+    }
+
+    @PutMapping("/public/categories/{categoryId}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long categoryId,
+                                                   @RequestBody Category updateCategory){
+        Category updatedCategory = categoryService.updateCategory(categoryId, updateCategory);
+        return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
+    }
+
+
+}
