1b821489efb096df1f4422011b51a2b7e277f967
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-16 07:53:10 -0400

    feat(app): Entity annotations

--- src/main/java/com/app/ecommerce/model/Category.java
+++ src/main/java/com/app/ecommerce/model/Category.java
@@ -1,12 +1,21 @@
 package com.app.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity(name = "categories")
+@NoArgsConstructor
 @AllArgsConstructor
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 }
