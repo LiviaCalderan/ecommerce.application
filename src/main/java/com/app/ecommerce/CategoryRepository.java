ce74fcb9cf8a915d4cd2f9fe016fcbefc6838d65
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-16 08:04:29 -0400

    feat(app): Category Repository created

---
+++ src/main/java/com/app/ecommerce/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.app.ecommerce;
+
+import com.app.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
