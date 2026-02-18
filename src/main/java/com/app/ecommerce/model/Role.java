f5f69e36eafe986c2b93768f0d98838685c023a1
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-18 18:30:35 -0400

    feat(app): User Entity Implemented

---
+++ src/main/java/com/app/ecommerce/model/Role.java
@@ -0,0 +1,24 @@
+package com.app.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name", length = 20)
+    private AppRole roleName;
+
+}
---
+++ src/main/java/com/app/ecommerce/model/User.java
@@ -0,0 +1,51 @@
+package com.app.ecommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "username")
+    private String userName;
+
+    @Email
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+                fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
+}
