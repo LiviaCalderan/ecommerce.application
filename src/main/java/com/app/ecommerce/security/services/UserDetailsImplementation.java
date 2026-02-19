75e2b74eb9168c103e7a1ffcbfde91d9665b33e7
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-19 14:18:12 -0400

    feat(app): Custom UserDetails

---
+++ src/main/java/com/app/ecommerce/security/services/UserDetailsImplementation.java
@@ -0,0 +1,93 @@
+package com.app.ecommerce.security.services;
+
+import com.app.ecommerce.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDetailsImplementation implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private UUID id;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    private static UserDetailsImplementation build(User user){
+        List<GrantedAuthority> authorityList = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
+                .collect(Collectors.toList());
+
+        return new UserDetailsImplementation(
+                user.getUserId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.getPassword(),
+                authorityList
+        );
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImplementation user = (UserDetailsImplementation) o;
+        return Objects.equals(id, user.id);
+    }
+}
