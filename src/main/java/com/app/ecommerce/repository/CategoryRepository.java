7dcef9ea248d514694d84dfe26b4b555f14858b4
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-19 14:50:39 -0400

    feat(app): UserRepository created and Repository Annotations

--- src/main/java/com/app/ecommerce/repository/CategoryRepository.java
+++ src/main/java/com/app/ecommerce/repository/CategoryRepository.java
@@ -2,7 +2,9 @@ package com.app.ecommerce.repository;
 
 import com.app.ecommerce.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Category findByCategoryName(String categoryName);
--- src/main/java/com/app/ecommerce/security/services/UserDetailsServiceImplementation.java
+++ src/main/java/com/app/ecommerce/security/services/UserDetailsServiceImplementation.java
@@ -1,6 +1,7 @@
 package com.app.ecommerce.security.services;
 
 import com.app.ecommerce.model.User;
+import com.app.ecommerce.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,7 +19,7 @@ public class UserDetailsServiceImplementation implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        User user = userRepository.findByUsername(username)
+        User user = userRepository.findByUserName(username)
                 .orElseThrow(
                         () -> new UsernameNotFoundException("User Not Found with Username: " + username));
         return UserDetailsImplementation.build(user);
