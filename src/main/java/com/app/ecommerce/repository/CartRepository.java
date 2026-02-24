e751805b21f31627e2ea64ac22bbec9eb0766ae4
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-24 07:05:42 -0400

    feat(app): PostgreSQL and Docker implementated

--- src/main/java/com/app/ecommerce/repository/CartRepository.java
+++ src/main/java/com/app/ecommerce/repository/CartRepository.java
@@ -14,6 +14,6 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     @Query("SELECT c FROM Cart c WHERE c.user.email = ?1 AND c.cartId= ?2")
     Cart findCartByEmailAndCartId(String email, Long cartId);
 
-    @Query("SELECT c FROM Cart c WHERE c JOIN FETCH c.cartItems ci JOIN FETCH ci.product p WHERE p.id = ?1")
+    @Query("SELECT DISTINCT c FROM Cart c JOIN FETCH c.cartItems ci JOIN FETCH ci.product p WHERE p.id = ?1")
     List<Cart> findCartsByProductId(Long productId);
 }
