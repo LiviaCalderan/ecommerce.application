c6dcbfb3a380b7187b3a8fc62b0cb257a3f9f931
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-25 13:32:04 -0400

    docs(swagger): Swagger - OpenAPI info added

--- src/main/java/com/app/ecommerce/config/SwaggerConfig.java
+++ src/main/java/com/app/ecommerce/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.app.ecommerce.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +24,14 @@ public class SwaggerConfig {
                 .addList("Bearer Authentication");
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("ECommerce API")
+                        .version("1.0")
+                        .description("Spring Boot ECommerce API")
+                        .license(new License().name("Apache 2.0"))
+                        .contact(new Contact()
+                                .name("Lívia Calderan")
+                                .url("https://github.com/LiviaCalderan")))
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication",  bearerScheme))
                 .addSecurityItem(bearerRequirement);
